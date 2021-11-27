e2288d6a143dc0b3d466da32ce9400042cf91f3a
bird348 <bird348@gmail.com>
2021-11-27 19:57:52 +0700

    edit env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    tpye: process.env.TPYE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URL,
-    token_uri: process.env.TOKEN_URL,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+    tpye: process.env.NEXT_PUBLIC_TPYE,
+    project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+    private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+    private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    auth_uri: process.env.NEXT_PUBLIC_AUTH_URL,
+    token_uri: process.env.NEXT_PUBLIC_TOKEN_URL,
+    auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL
   }
 }
