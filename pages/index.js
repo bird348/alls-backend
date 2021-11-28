71967b43030d6d1efb17888d6e1a814ed89b6d3b
bird348 <bird348@gmail.com>
2021-11-28 17:59:25 +0700

    edit key

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
     <>
       <div className="bg-success text-light text-center">
         <h3>Test api</h3>
+        <p className="text-danger">by bird348</p>
       </div>
     </>
   )
