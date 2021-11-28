57d3c87d584bb8a5a9e673e6344a396d47546346
bird348 <bird348@gmail.com>
2021-11-28 18:22:56 +0700

    edit key

--- utils/db/index.js
+++ utils/db/index.js
@@ -4,7 +4,7 @@ var env = {
   tpye: process.env.NEXT_PUBLIC_TPYE,
   project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
   private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
   client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
   client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
   auth_uri: process.env.NEXT_PUBLIC_AUTH_URL,
